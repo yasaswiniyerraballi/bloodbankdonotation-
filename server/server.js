0f97e89a96444a9ca10eb9a5df78ca101311f03b
yasaswiniyerraballi <130228073+yasaswiniyerraballi@users.noreply.github.com>
2024-10-19 20:45:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const path = require('path');
+
+require('dotenv').config();
+const dbconfig = require('./config/dbconfig');
+app.use(express.json());
+
+const userRoute = require('./routes/usersRoute')
+const inventoryRoute = require('./routes/inventoryRoute')
+const dashboardRoute = require('./routes/dashboardRoute')
+
+app.use('/api/users',userRoute);
+app.use('/api/inventory',inventoryRoute);
+app.use('/api/dashboard',dashboardRoute);
+
+app.listen(port,()=>console.log(`Node js is started at the ${port}`));
